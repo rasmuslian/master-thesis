09769d74cf0b06cefac244cec1b1566c6c5e6d8a
rasmuslian <lianrasmus@gmail.com>
2024-03-29 09:48:45 +0100

    Minor

--- download_stock_data.py
+++ download_stock_data.py
@@ -33,7 +33,11 @@ test_folder_exist = check_if_folder_exists(f"stock_data/test/{setup.test_tickers
 # Get training ticker list
 if not train_folder_exist:
     download_flow('train', setup.train_tickerslist, setup.train_start_date, setup.train_end_date)
+else:
+    print('Training stock data already exists')
 
 # Get test ticker list
 if not test_folder_exist:
     download_flow('test', setup.test_tickerslist, setup.test_start_date, setup.test_end_date)
+else:
+    print('Test stock data already exists')
