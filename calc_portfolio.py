4f4203e243f088ef02bfcb793ee39ef5cd4da930
rasmuslian <lianrasmus@gmail.com>
2024-04-08 20:13:26 +0200

    Minor

--- calc_portfolio.py
+++ calc_portfolio.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import setup
 
-'''--- Portfolio calculation ---'''
+'''--- Portfolio ---'''
 
 # Import csv from portfolios/{setup.tickeslist}.csv
 portfolio_df = pd.read_csv(f"portfolios/{setup.test_tickerslist}.csv", index_col=0)
@@ -22,6 +22,32 @@ portfolio_df['riskfree'] = riskfree_df['3 mån']/100
 
 # Add a new column that takes the annualized risk free rate and divides it by 360 to get the daily risk free rate. Append to new column called riskfree_daily
 portfolio_df['riskfree_daily'] = (1 + portfolio_df['riskfree']/100)**(1/360) - 1
+
+
+'''--- Benchmarks ---'''
+benchmark_df = pd.read_csv(f"portfolio_data/benchmarks/{setup.test_tickerslist}.csv", index_col=0)
+benchmark_df = benchmark_df.loc[setup.test_start_date:setup.test_end_date]
+
+portfolio_df['benchmark'] = benchmark_df['Close'].pct_change()
+
+# Calculate average returns
+average_portfolio_return = portfolio_df['portfolio'].mean()
+average_risk_free_return = portfolio_df['riskfree_daily'].mean()
+average_benchmark_return = portfolio_df['benchmark'].mean()
+
+# Calculate the portfolio's beta
+covariance = portfolio_df['portfolio'].cov(portfolio_df['benchmark'])
+benchmark_variance = portfolio_df['benchmark'].var()
+beta = covariance / benchmark_variance
+
+# Calculate Jensen's Alpha
+jensens_alpha = average_portfolio_return - (average_risk_free_return + beta * (average_benchmark_return - average_risk_free_return))
+
+print("Jensen's Alpha:", jensens_alpha)
+
+
+'''--- Beta, Alpha and Sharpe ---'''
+
 
 
 '''--- Save ---'''
