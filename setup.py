54f6aa5e0d6635b26d94a05298e2d868c99b5dfc
rasmuslian <lianrasmus@gmail.com>
2024-06-07 16:27:11 +0200

    Minor

--- setup.py
+++ setup.py
@@ -5,19 +5,16 @@ train_end_date = '2020-06-30'
 validate_start_date = '2020-07-01'
 validate_end_date = '2021-12-31'
 
-test_start_date = '2016-01-01'
-test_end_date = '2023-12-31'
-# test_start_date = '2022-01-01'
-# test_end_date = '2023-12-31'
+test_start_date = '2024-05-13'
+test_end_date = '2023-06-06'
 
 # Tickers list
-train_tickerslist = 'sp500'
+train_tickerslist = 'stockallshares'
 test_tickerslist = 'stockallshares'
 
 # Tickers that exist the whole period, to ensure date consistency
-
-train_ticker_trading_base = 'AAPL' # sp500
-# train_ticker_trading_base = 'ABB.ST' # stockallshares
+# train_ticker_trading_base = 'AAPL' # sp500
+train_ticker_trading_base = 'ABB.ST' # stockallshares
 
 validate_ticker_trading_base = 'AAPL' # sp500
 # validate_ticker_trading_base = 'ABB.ST' # stockallshares
@@ -37,5 +34,5 @@ batch_size = 32
 learning_rate = 0.001
 
 # Test model
-test_model_name = 'model_zwicde__actep6_apr20-1330_sp500_1d_tf_efficientnet_b7_ns_maxep100_bs32_lr0001'
+test_model_name = 'model_oburmo__actep6_apr06-0435_stockallshares_1d_tf_efficientnet_b7_ns_maxep100_bs32_lr0001'
 
